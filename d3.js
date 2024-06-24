adff4ed817df89a99dab58e8ac693729c3798e6d
Irene Goff <ireneegoff@gmail.com>
2024-06-24 18:10:17 -0400

    Enhance styling for the data visualization dashboard.

--- d3.js
+++ d3.js
@@ -59,3 +59,70 @@ document.addEventListener('DOMContentLoaded', function () {
             .style("padding", "5px")
             .style("border", "1px solid #ccc")
             .style("border-radius", "3px");
+
+            svg.selectAll("circle")
+            .data(data)
+            .enter().append("circle")
+            .attr("r", 5)
+            .attr("cx", d => x(d.date))
+            .attr("cy", d => y(d.value))
+            .on("mouseover", function (event, d) {
+                tooltip.html(`Stock: ${d.stock}<br>Date: ${d3.timeFormat("%Y-%m-%d")(d.date)}<br>Value: ${d.value}`)
+                    .style("visibility", "visible");
+            })
+            .on("mousemove", function (event) {
+                tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 10) + "px");
+            })
+            .on("mouseout", function () {
+                tooltip.style("visibility", "hidden");
+            });
+
+        const stocks = [...new Set(data.map(d => d.stock))];
+        const stockSelect = document.getElementById('stock-select');
+        stocks.forEach(stock => {
+            const option = document.createElement('option');
+            option.value = stock;
+            option.text = stock;
+            stockSelect.appendChild(option);
+        });
+
+        document.getElementById('filter-button').addEventListener('click', () => {
+            const selectedStock = stockSelect.value;
+            const startDate = new Date(document.getElementById('start-date').value);
+            const endDate = new Date(document.getElementById('end-date').value);
+            const filteredData = data.filter(d => d.stock === selectedStock && d.date >= startDate && d.date <= endDate);
+
+            x.domain(d3.extent(filteredData, d => d.date));
+            y.domain([0, d3.max(filteredData, d => d.value)]).nice();
+
+            svg.selectAll("*").remove();
+
+            svg.append("g").call(xAxis);
+            svg.append("g").call(yAxis);
+
+            svg.append("path")
+                .datum(filteredData)
+                .attr("fill", "none")
+                .attr("stroke", "steelblue")
+                .attr("stroke-width", 1.5)
+                .attr("d", line);
+
+            svg.selectAll("circle")
+                .data(filteredData)
+                .enter().append("circle")
+                .attr("r", 5)
+                .attr("cx", d => x(d.date))
+                .attr("cy", d => y(d.value))
+                .on("mouseover", function (event, d) {
+                    tooltip.html(`Stock: ${d.stock}<br>Date: ${d3.timeFormat("%Y-%m-%d")(d.date)}<br>Value: ${d.value}`)
+                        .style("visibility", "visible");
+                })
+                .on("mousemove", function (event) {
+                    tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 10) + "px");
+                })
+                .on("mouseout", function () {
+                    tooltip.style("visibility", "hidden");
+                });
+        });
+    });
+});
